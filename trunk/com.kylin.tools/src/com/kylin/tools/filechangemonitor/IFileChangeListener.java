5fb4c961f848cf714594ac129a7ea19797efee66
Kylin <songzhiqi_1214@yahoo.com.cn>
2012-07-06 18:30:14 +0800

    remove previous eclipse test project

--- trunk/com.kylin.tools/src/com/kylin/tools/filechangemonitor/IFileChangeListener.java
+++
@@ -1,8 +0,0 @@
-package com.kylin.tools.filechangemonitor;
-
-import java.io.File;
-
-public interface IFileChangeListener {
-
-	public abstract void addListener(IFileChangeHandler handler, File file);
-}
