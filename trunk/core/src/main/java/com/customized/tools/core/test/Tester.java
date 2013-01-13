4a862cfd85cb514f11c058cb05beba55e2edea5a
Kylin <songzhiqi_1214@yahoo.com.cn>
2013-01-13 22:14:51 +0800

    add cst core module

---
+++ trunk/core/src/main/java/com/customized/tools/core/test/Tester.java
@@ -0,0 +1,12 @@
+package com.customized.tools.core.test;
+
+import java.io.File;
+
+public class Tester {
+
+	public static void main(String[] args) {
+
+		System.out.println(File.separator);
+	}
+
+}
--- trunk/po/src/main/java/com/customized/tools/po/version/Version.java
+++ trunk/po/src/main/java/com/customized/tools/po/version/Version.java
@@ -6,13 +6,21 @@ public class Version {
 	
 	public static final String V_2 = "2.0";
 	
+	public static final String V_CURRENT = V_2 ;
+	
 	public static final String NAME = "CustomizedTools";
 	
+	public static final String VERSION_STRING = NAME + " 'KylinSoong' " + V_CURRENT ;
+	
 	public static String version() {
-		return V_2 ;
+		return V_CURRENT ;
 	}
 	
 	public static String name() {
 		return NAME ;
+	}
+	
+	public static String versionString() {
+		return VERSION_STRING ;
 	}
 }
